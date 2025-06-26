209b7ca76538a3cd618335ad947862a31bdab3af
AlonGil-Ad <alon.gilad.cs@gmail.com>
2025-06-26 15:38:38 +0300

    Added more comments

--- example.py
+++ example.py
@@ -8,20 +8,21 @@ from time import sleep
 from SimpleMocap import SimpleMocap
 
 if __name__ == "__main__":
+    # Optional logger usage. The client inherits the root logger.
+    # Default logging level for the client is INFO
     logger = logging.getLogger()
+    logging.basicConfig(encoding='utf-8')
     # Available logging levels: DEBUG, INFO, WARNING, ERROR, CRITICAL
     logger.setLevel('DEBUG')
-    logging.basicConfig(encoding='utf-8')
     # Instantiate and starts the client. Change local_ip to your IP address
     mocap = SimpleMocap(local_ip="132.68.35.50", server_ip="132.68.35.2")
 
     for i in range(5):
-        print(f'Frame number: {mocap.get_frame_number()}')
-        print(f'Target: {mocap.get_location("Target")}')
-        print(f'Target: {mocap.get_location(603)}')
-        print(f'Tello2: {mocap.get_location("Tello2")}')
-        print(f'Tello3: {mocap.get_location("Tello3")}')
+        print(f'Frame number: {mocap.get_frame_number()}')                      # Get frame number
+        print(f'Target: {mocap.get_location("Target")}')                        # Get rigid body "Target" by name
+        print(f'Target: {mocap.get_location(603)}')                             # Get rigid body "Traget" by ID
+        print(f'NonexistantBody: {mocap.get_location("NonexistantBody")}')      # Get undefined rigid body
         sleep(1)
 
-    mocap.shutdown()
+    mocap.shutdown()        # Always call shutdown before script end
 
